723f553a6be8d6446065e9e1aa56ed28a76c4132
Mohammed Alhaddar <moalhaddar@gmail.com>
2024-09-20 14:10:39 +0300

    Added raygui full repo as external

--- src/main.c
+++ src/main.c
@@ -4,22 +4,31 @@
 #define RAYGUI_IMPLEMENTATION
 #include "raygui.h"
 
-double weight1_1, weight2_1;
-double weight1_2, weight2_2;
+float weight1_1, weight2_1;
+float weight1_2, weight2_2;
+
+int DrawWeightControls() {
+    Vector2 anchor01 = { 56, 48 };
+    GuiGroupBox((Rectangle){ anchor01.x + 0, anchor01.y + 0, 256, 200 }, "Weights");
+    GuiSlider((Rectangle){ anchor01.x + 64, anchor01.y + 32, 136, 24 }, "W1,1", NULL, &weight1_1, 0, 100);
+    GuiSlider((Rectangle){ anchor01.x + 64, anchor01.y + 72, 136, 24 }, "W1,2", NULL, &weight1_2, 0, 100);
+    GuiSlider((Rectangle){ anchor01.x + 64, anchor01.y + 112, 136, 24 }, "W2,1", NULL, &weight2_1, 0, 100);
+    GuiSlider((Rectangle){ anchor01.x + 64, anchor01.y + 152, 136, 24 }, "W2,2", NULL, &weight2_2, 0, 100);
+}
 
 int main() {
     int factor = 100;
+    char* styles_path = "./src/external/raygui-4.0/styles/cyber/style_cyber.rgs";
     InitWindow(16 * factor, 9 * factor, "Neural Network");
+    GuiLoadStyle(styles_path);
+    GuiSetStyle(DEFAULT, TEXT_SIZE, 20);
 
-    float value = 0.5;
+    float value = 0.0;
 
     while (!WindowShouldClose()) {
         ClearBackground(BLACK);
         BeginDrawing();
-        // GuiGroupBox((Rectangle){ 66, 24, 276, 312 }, "STANDARD");
-        GuiSlider((Rectangle){ 96, 48, 216, 32 }, TextFormat("%0.2f", value), NULL, &value, 0.0f, 1.0f);
-        // value = GuiVerticalSlider((Rectangle){ 120, 120, 24, 192 }, TextFormat("%0.2f", value), NULL, value, 0.0f, 1.0f);
-        // value = GuiVerticalSliderBar((Rectangle){ 264, 120, 24, 192 }, TextFormat("%0.2f", value), NULL, value, 0.0f, 1.0f);
+        DrawWeightControls();
 
         EndDrawing();
     }
