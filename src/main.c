36be126bb8cdc65d3f84c5f6025660a8439e74d6
Mohammed Alhaddar <moalhaddar@gmail.com>
2024-09-20 16:58:36 +0300

    Proper graph drawing

--- src/main.c
+++ src/main.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 #include <raylib.h>
+#include <raymath.h>
+#include <math.h>
 
 #define RAYGUI_IMPLEMENTATION
 #include "raygui.h"
+
+#define WORLD_WIDTH 10000
+#define WORLD_HEIGHT 10000
+#define GRID_STEP 100
 
 float weight1_1, weight2_1;
 float weight1_2, weight2_2;
@@ -10,10 +16,77 @@ float weight1_2, weight2_2;
 int DrawWeightControls() {
     Vector2 anchor01 = { 56, 48 };
     GuiGroupBox((Rectangle){ anchor01.x + 0, anchor01.y + 0, 256, 200 }, "Weights");
-    GuiSlider((Rectangle){ anchor01.x + 64, anchor01.y + 32, 136, 24 }, "W1,1", NULL, &weight1_1, 0, 100);
-    GuiSlider((Rectangle){ anchor01.x + 64, anchor01.y + 72, 136, 24 }, "W1,2", NULL, &weight1_2, 0, 100);
-    GuiSlider((Rectangle){ anchor01.x + 64, anchor01.y + 112, 136, 24 }, "W2,1", NULL, &weight2_1, 0, 100);
-    GuiSlider((Rectangle){ anchor01.x + 64, anchor01.y + 152, 136, 24 }, "W2,2", NULL, &weight2_2, 0, 100);
+    GuiSlider((Rectangle){ anchor01.x + 64, anchor01.y + 32, 136, 24 }, "W1,1", NULL, &weight1_1, -1, 1);
+    GuiSlider((Rectangle){ anchor01.x + 64, anchor01.y + 72, 136, 24 }, "W1,2", NULL, &weight1_2, -1, 1);
+    GuiSlider((Rectangle){ anchor01.x + 64, anchor01.y + 112, 136, 24 }, "W2,1", NULL, &weight2_1, -1, 1);
+    GuiSlider((Rectangle){ anchor01.x + 64, anchor01.y + 152, 136, 24 }, "W2,2", NULL, &weight2_2, -1, 1);
+}
+
+int classify(float input_1, float input_2) {
+    double output_1 = input_1 * weight1_1 + input_2 * weight2_1;
+    double output_2 = input_1 * weight1_2 + input_2 * weight2_2;
+
+    return (output_1 > output_2) ? 0 : 1;
+}
+
+void visualize(float graphX, float graphY) {
+    float predictedClass = classify(graphX, graphX);
+
+    if (predictedClass == 0) {
+        DrawPixel(graphX, graphY, BLUE);
+    } else {
+        DrawPixel(graphX, graphY, RED);
+    }
+}
+
+void DrawInfiniteGrid(Camera2D camera) {
+    Vector2 cameraWorldPos = GetScreenToWorld2D((Vector2){0, 0}, camera);
+    Vector2 cameraWorldEndPos = GetScreenToWorld2D((Vector2){GetScreenWidth(), GetScreenHeight()}, camera);
+
+    int startX = ((int)(cameraWorldPos.x / GRID_STEP) - 1) * GRID_STEP;
+    int endX = ((int)(cameraWorldEndPos.x / GRID_STEP) + 1) * GRID_STEP;
+    int startY = ((int)(cameraWorldPos.y / GRID_STEP) - 1) * GRID_STEP;
+    int endY = ((int)(cameraWorldEndPos.y / GRID_STEP) + 1) * GRID_STEP;
+
+    for (int x = startX; x <= endX; x += GRID_STEP) {
+        DrawLine(x, startY, x, endY, GRAY);
+        char numStr[10];
+        snprintf(numStr, sizeof(numStr), "%d", x / GRID_STEP);
+        DrawText(numStr, x + 5, 5, 20 / camera.zoom, RAYWHITE);
+    }
+
+    for (int y = startY; y <= endY; y += GRID_STEP) {
+        DrawLine(startX, y, endX, y, GRAY);
+        if (y != 0) {  // Avoid drawing 0 twice
+            char numStr[10];
+            snprintf(numStr, sizeof(numStr), "%d", -y / GRID_STEP);
+            DrawText(numStr, 5, y + 5, 20 / camera.zoom, RAYWHITE);
+        }
+    }
+
+    DrawLine(startX, 0, endX, 0, RED);
+    DrawLine(0, startY, 0, endY, GREEN);
+}
+
+void DrawFunction(Camera2D camera) {
+    Vector2 cameraWorldPos = GetScreenToWorld2D((Vector2){-1000, -1000}, camera);
+    Vector2 cameraWorldEndPos = GetScreenToWorld2D((Vector2){GetScreenWidth() * 2, GetScreenHeight() * 2}, camera);
+
+    int startX = cameraWorldPos.x / GRID_STEP;
+    int endX = cameraWorldEndPos.x / GRID_STEP;
+    printf("startX: %d, endX: %d\n", startX, endX);
+
+    int res = 10;
+    Vector2 points[(endX - startX + 1) * res];
+    int pointCount = 0;
+
+    for (float x = startX; x <= endX; x+=(1.0f/res)) {
+        float y = 0.2 * powf(x, 4) + 0.1 * powf(x, 3) - powf(x, 2) + 2;
+        points[pointCount] = (Vector2){(float)x * GRID_STEP, -y * GRID_STEP};
+        pointCount++;
+    }
+
+    DrawLineStrip(points, pointCount, YELLOW);
 }
 
 int main() {
@@ -23,11 +96,67 @@ int main() {
     GuiLoadStyle(styles_path);
     GuiSetStyle(DEFAULT, TEXT_SIZE, 20);
 
-    float value = 0.0;
+    Vector2 target = {0, 0};
+    Vector2 offset = {0, 0};
+    Vector2 anchor = {0, 0};
+    Vector2 mouseDelta = {0, 0};
+    int dragging = 0;
+    float zoom = 1.0f;
+    float targetZoom = 1.0f;
 
     while (!WindowShouldClose()) {
-        ClearBackground(BLACK);
+        int screenWidth = GetScreenWidth();
+        int screenHeight = GetScreenHeight();
+        mouseDelta = GetMouseDelta();
+        
+        Camera2D camera = {
+            .target = target,
+            .offset = (Vector2){screenWidth/2.0f, screenHeight/2.0f},
+            .rotation = 0,
+            .zoom = zoom,
+        };
+
+        float wheel = GetMouseWheelMove();
+        if (wheel != 0) {
+            Vector2 mouseWorldPos = GetScreenToWorld2D(GetMousePosition(), camera);
+            targetZoom += wheel * 0.05f;
+            if (targetZoom < 0.1f) targetZoom = 0.1f;
+            if (targetZoom > 10.0f) targetZoom = 10.0f;
+            zoom = Lerp(zoom, targetZoom, 0.1f);
+            camera.zoom = zoom;
+            Vector2 mouseWorldPosNew = GetScreenToWorld2D(GetMousePosition(), camera);
+            target = Vector2Add(target, Vector2Subtract(mouseWorldPos, mouseWorldPosNew));
+        } else {
+            zoom = Lerp(zoom, targetZoom, 0.1f);
+            camera.zoom = zoom;
+        }
+
+        if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
+            dragging = 1;
+            anchor = GetScreenToWorld2D(GetMousePosition(), camera);
+        } else if (IsMouseButtonReleased(MOUSE_BUTTON_LEFT)) {
+            dragging = 0;
+        }
+
+        if (dragging) {
+            Vector2 newPosition = GetScreenToWorld2D(GetMousePosition(), camera);
+            target = Vector2Subtract(target, Vector2Subtract(newPosition, anchor));
+        }
+
+        ClearBackground(GetColor(GuiGetStyle(DEFAULT, BACKGROUND_COLOR)));
         BeginDrawing();
+            DrawText(TextFormat("Target: %.2f, %.2f", target.x, target.y), 0, 0, 20, RAYWHITE);
+            DrawText(TextFormat("Mouse Delta: %.2f, %.2f", mouseDelta.x, mouseDelta.y), 0, 40, 20, RAYWHITE);
+            DrawText(TextFormat("Zoom: %.2f", zoom), 0, 80, 20, RAYWHITE);
+
+            if (dragging) {
+                DrawCircleV(GetWorldToScreen2D(anchor, camera), 10, YELLOW);
+            }
+            BeginMode2D(camera);
+                DrawInfiniteGrid(camera);
+                DrawFunction(camera);
+            EndMode2D();
+
         DrawWeightControls();
 
         EndDrawing();
